a781b8c04ac34bd081e3693ddacd83373f00b08f
Marcelo Almeida <marceloalmeida98@gmail.com>
2020-05-15 00:14:46 -0400

    Beginning buildMap

---
+++ map/triggerMap.js
@@ -0,0 +1,15 @@
+$('#trigger').onclick = () => {
+    $.ajax({
+        type:'get',
+        url:'../main.py',
+        cache:false,
+        async:'asynchronous',
+        dataType:'json',
+        success: function(data) {
+          console.log(JSON.stringify(data))
+        },
+        error: function(request, status, error) {
+          console.log("Error: " + error)
+        }
+    });
+}

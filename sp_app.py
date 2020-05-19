538ee3903e9693e3db2983272c4016bc29a9b150
Marcelo Almeida <marceloalmeida98@gmail.com>
2020-05-19 17:30:31 -0400

    Added abort status

--- sp_app.py
+++ sp_app.py
@@ -8,18 +8,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def init():
-    
-    render_template('main.html')
+    export SPOTIPY_CLIENT_ID='your-spotify-client-id' # input your client id
+    export SPOTIPY_CLIENT_SECRET='your-spotify-client-secret' # input your client secret
+    export SPOTIPY_REDIRECT_URI='your-app-redirect-uri' # input your redirect uri, e.g. localhost:8000/callback
     return render_template('login.html')
 
 @app.route('/main')
 def main():
     return render_template('main.html')
 
-@app.route('/login', methods=['GET', 'POST'])
-def login(name=None):
-    # cannot see URL parameters in POST method
-    # if request.method == 'POST':
+@app.route('/login')
+def login():
     username = request.args.get('username')
     environ['user'] = username
 
@@ -29,8 +28,12 @@ def login(name=None):
             environ['token'] = token
             response = 'User log in successful.'
             return jsonify(response)
-    # else:
-        # return 'Wrong method! Use POST.'
+        else:
+            description = 'Unable to authenticate!'
+            return abort(401, description)
+    else:
+        description = 'Not username provided!'
+        return abort(401, description)
 
 @app.route('/run', methods=['GET'])
 def run():
@@ -43,13 +46,15 @@ def run():
             token = environ.get('token')
 
             data = passport.run(username, token, playlist_name)
-            return jsonify(data)  # serialize and use JSON headers
+            if (type(data) == str):
+                abort(404, description)
+            else:
+                return jsonify(data)  # serialize and use JSON headers
         else:
             description='Error! No playlist provided.'
             abort(404, description)
-            # return res
     else:
         abort(405)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=8888)
+    app.run(debug=True, port=8000)
--- src/main.py
+++ src/main.py
@@ -3,19 +3,17 @@ import spotipy.util as util
 import src.fetchSpotify as spot
 import src.fetchCountry as musix
 from os import environ
-# import time
 
 def run(username, token, playlist_name):
-    # tic = time.perf_counter()
     if not username or not playlist_name:
         return 'Whoops, need your username and playlist name!'
 
     if token:
         artistDict = spot.getUserArtists(username, token, playlist_name)
+        
         if len(artistDict) == 0:
             return 'Unable to fetch playlist data! Playlist is either empty or private.'
-        # countryFreq['unknown'] = 1
-        # consider increasing by artist frequency
+
         countryFreq = dict()
         num = 0
         for artist in artistDict:
@@ -26,13 +24,7 @@ def run(username, token, playlist_name):
             else:
                 countryFreq[country] = 1
 
-        # toc = time.perf_counter()
-        # print("Total runtime: ", toc-tic)
         return countryFreq
-        # if "unknown" in countryFreq: print("Unknown: {}, total: {}", countryFreq["unknown"], num)
     else:
         print("Can't get token for", username)
         return "Unable to compile your passport! Please log in."
-
-if __name__ == '__main__':
-    run('1289838511', 'Fluorescent Adolescent')

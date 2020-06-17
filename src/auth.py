5b7ad4e1d63abf7354775044db30ef3f4c42e53c
Marcelo Almeida <marceloalmeida98@gmail.com>
2020-06-17 16:48:12 -0400

    Updated gitignore

--- src/auth.py
+++ src/auth.py
@@ -5,14 +5,8 @@ def auth():
     # fetch username from environment
     username = environ['user']
     
-    # scope = 'playlist-read-private'
-    # token = util.prompt_for_user_token(username, scope)
     token = util.prompt_for_user_token(username)
-    # token = util.prompt_for_user_token(username,
-    #                                     scope,
-    #                                     client_id='your-spotify-client-id',
-    #                                     client_secret='your-spotify-client-secret',
-    #                                     redirect_uri='your-app-redirect-url')
+    
     if token:
         return token
     else:
